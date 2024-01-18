a76f70c60e3c67cb0940d106dff5ea27fc46a064
oof123457 <153766288+oof123457@users.noreply.github.com>
2024-01-18 21:45:02 +0100

    New song
    Otaku

--- js/hanabie/fnc_data_v2.js
+++ js/hanabie/fnc_data_v2.js
@@ -65,8 +65,9 @@ var ary_CharacterData = [
   [1, "Today’s Good Day & So Epic", 			[0,0,1,0,0,0,0,0,0,0], "hanabiesong/reborn.jpg"],
 
   [1, "LOVE  Ranbu",                 			[0,0,0,0,1,0,0,0,0,0], "hanabiesong/loveranb.jpg"],
+  [1, "O･TA･KUラブリー伝説",                 			[0,0,0,0,1,0,0,0,0,0], "hanabiesong/otaku.jpg"],
   [1, "Babylon",                 			[0,0,0,1,0,0,0,0,0,0], "hanabiesong/crashover.jpg"],
-  [1, "Sweetskiller",                 			[0,0,0,1,0,0,0,0,0,0], "hanabiesong/crashover.jpg"],
+  [1, "Sweetskiller",                 			[0,0,0,1,0,0,0,0,0,0], "hanabiesong/crashover.jpg"],    
   
   [1, "Cherry Blossoms Are Blooming Instrumental", 			[1,0,0,0,0,0,0,0,0,0], "hanabiesong/cherry.jpg"],
   [1, "Envy",                 					[1,0,0,0,0,0,0,0,0,0], "hanabiesong/cherry.jpg"],
